2dbea736b974533dea67771de1b034288823a65a
Mathias Latournerie <jidewe@gmail.com>
2020-09-12 11:39:05 +0200

    clean up

--- module/main.js
+++ module/main.js
@@ -138,12 +138,12 @@ Hooks.on('createChatMessage', (chatMessage) => {
 
     chatMessage._dice3danimating = true;
     Hooks.callAll("diceSoNiceRollStart", chatMessage.id);
-    let rollPromise = game.dice3d.showForRoll(chatMessage.roll, chatMessage.user).then(displayed => {
+    game.dice3d.showForRoll(chatMessage.roll, chatMessage.user).then(displayed => {
         delete chatMessage._dice3danimating;
         $(`#chat-log .message[data-message-id="${chatMessage.id}"]`).show();
         Hooks.callAll("diceSoNiceRollComplete", chatMessage.id);
         ui.chat.scrollBottom();
-    });;
+    });
 });
 
 /**
